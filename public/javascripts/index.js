6176fb09300fa8d328af09a7d9bdddd65141e5b0
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-29 14:30:53 -0700

    Add sessionStorage to project

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const d3 = require("d3");
 
 
 const flightCategoryPage = () =>{
@@ -29,7 +30,11 @@ const sendFlightApiRequest = (e) => {
         "distance_unit": "mi"
         })
         .then((response) => {
-            console.log(response); 
+            //console.log(response);
+            sessionStorage.setItem(`Trip ${sessionStorage.length + 1}`, JSON.stringify(response.data.data.attributes))
+            console.log("This is the session storage",sessionStorage);
+            carbonfootprintChart(response.data.data.attributes);
+
         })
         .catch(function (error) {
             console.log(error);
@@ -37,20 +42,53 @@ const sendFlightApiRequest = (e) => {
 
 }
 
+const carbonfootprintChart = () => {
+    let dataArr = [];
+    for(let i = 0; i < sessionStorage.length; i++){
+        let trip = JSON.parse(sessionStorage.getItem(`Trip ${i + 1}`));
+        dataArr.push(trip)
+    }
+    console.log(dataArr);
+    const margin = 60;
+    const width = 800 - 2 * margin;
+    const height = 600 - 2 * margin;
+    const svg = d3.select('svg')
 
 
+    const chart = svg.append('g').attr('transform', `translate(${margin}, ${margin})`);
+    const yScale = d3.scaleLinear().range([height, 0]).domain([0, 100]);
+    chart.append('g').call(d3.axisLeft(yScale));
 
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(dataArr.map((trip) => `${trip.legs[0].departure_airport} to ${trip.legs[0].destination_airport}`))
+        .padding(0.2);
 
+    chart.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(xScale));
 
+    chart.selectAll()
+        .data(dataArr)
+        .enter()
+        .append('rect')
+        .attr('x', (s) => xScale(s))
+        .attr('y', (s) => {
+            if(sessionStorage.length === 0){
+                return yScale(0);
+            }else{
+             return yScale(s.carbon_lb)
+            }
+        })
+        .attr('height', (s) => height - (sessionStorage.length !== 0 ? yScale(s.carbon_lb) : yScale(0)))
+        .attr('width', xScale.bandwidth())
 
-
-
+}
 
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     flightCategoryPage();
+    carbonfootprintChart();
     console.log("I'm inside event listener")
     // axios.post('/carbonfootprint',{
     //     "type": "flight",
