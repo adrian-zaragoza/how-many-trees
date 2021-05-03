50d2f0d1df52817d8ce296ceec1844f29870c022
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-05-02 22:40:19 -0700

    Ready for Heroku

--- app.js
+++ app.js
@@ -54,6 +54,13 @@ app.post('/carbonfootprint', jsonParser, (request, response) => {
 //     });
 // });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 app.listen(PORT, () => {
   console.log(__dirname);
   console.log(`listening on ${PORT}`)
