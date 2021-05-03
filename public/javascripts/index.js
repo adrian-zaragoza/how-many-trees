57265fddfa959898dda435e8e81c7a136dff9d91
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-05-02 22:20:40 -0700

    Finish Styling homepage

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,7 +8,7 @@ const flightCategoryPage = () =>{
     titleEle.appendChild(titleText);
     
     let dataForm = document.querySelector(".carbonfootprint-form");
-    dataForm.innerHTML = "<label>Number of Passengers<input type=number min=1 id='passenger-num'></label><br>"
+    dataForm.innerHTML = "<label>Number of Passengers<input type=number min=1 id='passenger-num'></label>"
     dataForm.innerHTML += "<p>For the airports, please use the 3-Letter Location Code for the airport found <a href='https://www.iata.org/en/publications/directories/code-search/' target='_blank'>HERE</a></p>"
     dataForm.innerHTML += "<label>Departure Airport<input type='text' placeholder='SFO' id='departure-airport'></label>"
     dataForm.innerHTML += "<label>Destination Airport<input type='text' placeholder='JFK' id='destination-airport'></label>"
@@ -58,8 +58,8 @@ const carbonfootprintChart = () => {
     }
     console.log(dataArr);
     
-    const width = 800;
-    const height = 600;
+    const width = 500;
+    const height = 300;
     const margin = {top: 50, bottom: 50, left: 50, right: 50}
 
     const svg = d3.select('#category-chart')
@@ -85,8 +85,9 @@ const carbonfootprintChart = () => {
             .scale(y)
             .tickSize(-width, 0, 0)
             .tickFormat(''))
+
     //This is for the bars
-    svg.append('g')
+    const barGroup = svg.append('g')
         .attr('fill', 'green')
         .selectAll('rect')
         .data(dataArr)
@@ -94,25 +95,65 @@ const carbonfootprintChart = () => {
             .attr('x', (dataArr, i) => x(i))
             .attr('y', (dataArr) => y(dataArr.carbon_lb))
             .attr('height', (dataArr) => y(0) - y(dataArr.carbon_lb))
-            .attr('width', x.bandwidth());
+            .attr('width', x.bandwidth())
+            .attr('class', 'bar')
+    //         .on('mouseenter', function (actual, i) {
+    //             d3.selectAll('.bar')
+    //             .attr('opacity', 0)
+
+    //   })
+
+    // Trying to add the carbon lb value to the bar
+    // barGroup.append('g')
+    //     .append('text')
+    //     .attr('class', 'carbon-lbs')
+    //     .attr('x', (dataObj, i) => x(i))
+    //     .attr('y', (dataObj) => y(dataObj.carbon_lb))
+    //     .attr('text-anchor', 'middle')
+    //     .text((dataObj) => `${dataObj.carbon_lb}lbs`)
 
     function xAxis(g){
         g.attr('transform', `translate(0, ${height - margin.bottom})`)
             .call(d3.axisBottom(x).tickFormat(i =>{
                 let dataObj = dataArr[i];
-                let axisName = `${dataObj.legs[0].departure_airport} to ${dataObj.legs[0].destination_airport} `;
+                let axisName = `${dataObj.legs[0].departure_airport} to ${dataObj.legs[0].destination_airport}`;
                 return axisName;
             }))
-            .attr('font-size', '12px')
+            .attr('font-size', '8px')
     }
 
     function yAxis(g){
         g.attr('transform', `translate(${margin.left}, 0)`)
             .call(d3.axisLeft(y).tickFormat(null, dataArr.format))
+            .attr('font-size', '8px')
     }
 
     svg.append('g').call(yAxis);
     svg.append('g').call(xAxis);
+
+    //Y-axis label
+    svg.append('text')
+        .attr('x', -(height / 2))
+        .attr('y', (margin.left - 10) / 2.4)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Carbon Footprint in lbs')
+        .attr('font-size', '11px')
+
+    //X-axis label
+  svg.append("text")             
+      .attr("transform", `translate(${width / 2}, ${height - 20})`)
+      .style("text-anchor", "middle")
+      .text("Roundtrip Flight Destinations")
+      .attr('font-size', '11px')
+
+    //Chart tile label
+    // svg.append('text')
+    //     .attr('x', width / 2 + margin.bottom)
+    //     .attr('y', 40)
+    //     .attr('text-anchor', 'middle')
+    //     .text('Rountrip Flights Carbon Footprint')
+    //     .attr('font-size', '17px')
 
 
     svg.node();
