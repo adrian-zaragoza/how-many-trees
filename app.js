be8f688dff5a5122be492ec4a3ac262a9f2621e4
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-05-03 08:31:24 -0700

    Update app

--- app.js
+++ app.js
@@ -54,14 +54,16 @@ app.post('/carbonfootprint', jsonParser, (request, response) => {
 //     });
 // });
 
+
+
+app.listen(PORT, () => {
+  console.log(__dirname);
+  console.log(`listening on ${PORT}`)
+})
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
-
-app.listen(PORT, () => {
-  console.log(__dirname);
-  console.log(`listening on ${PORT}`)
-})
